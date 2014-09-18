9d099f2c9297363b933b1a9a5a11fe46062bb764
Luis Alberto Ruiz <luisalbertoruiz.geek@gmail.com>
2014-09-17 23:11:28 -0500

    Agregando archivos faltantes

---
+++ SistemadeControlPoliciaco/SplashScreen.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SistemadeControlPoliciaco
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pgbCargando = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.tiempo = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pgbCargando
+            // 
+            this.pgbCargando.Location = new System.Drawing.Point(105, 153);
+            this.pgbCargando.Name = "pgbCargando";
+            this.pgbCargando.Size = new System.Drawing.Size(266, 23);
+            this.pgbCargando.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(385, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Versión 1.0.0";
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(13, 236);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(0, 17);
+            this.lblMensaje.TabIndex = 2;
+            // 
+            // tiempo
+            // 
+            this.tiempo.Tick += new System.EventHandler(this.tiempo_Tick);
+            // 
+            // splashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pgbCargando);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "splashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pgbCargando;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Timer tiempo;
+    }
+}
+
