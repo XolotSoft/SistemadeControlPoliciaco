d54ab3c35229651bf2188de281a0c313beb8452b
Luis Alberto Ruiz <luisalbertoruiz.geek@gmail.com>
2014-09-20 11:21:19 -0500

    Modificacion DB a ManejoBD con nuevas funciones

--- SistemadeControlPoliciaco/Clases/ManejoBD.cs
+++ SistemadeControlPoliciaco/Clases/ManejoBD.cs
@@ -9,7 +9,7 @@ using System.Data;
 
 namespace SistemadeControlPoliciaco
 {
-     public class DB
+     public class ManejoBD
     {
         public SqlCommand cmd;
         public SqlDataAdapter da;
@@ -17,9 +17,28 @@ namespace SistemadeControlPoliciaco
         public DataSet ds = new DataSet();
         public SqlDataReader dr;
 
-        public void buscar(string campos, string tabla)
+        public void buscar(string sql)
         {
-            string sql = "SELECT"+campos+"FROM"+tabla;
+            cmd = new SqlCommand(sql, Conexion.conectar());
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            da.Fill(ds);
+            Conexion.desconectar();
+        }
+
+        public void buscarg(string campos, string tabla)
+        {
+            string sql = "SELECT "+campos+" FROM "+tabla;
+            cmd = new SqlCommand(sql, Conexion.conectar());
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            da.Fill(ds);
+            Conexion.desconectar();
+        }
+
+        public void buscare(string campos, string tabla, string columna, string valor)
+        {
+            string sql = "SELECT " + campos + " FROM " + tabla+" WHERE "+columna+" = "+valor;
             cmd = new SqlCommand(sql, Conexion.conectar());
             da = new SqlDataAdapter(cmd);
             da.Fill(dt);
@@ -29,7 +48,7 @@ namespace SistemadeControlPoliciaco
 
         public bool insertar(string tabla, string columnas, string valores)
         {
-            string sql = "INSERT INTO"+tabla+"("+columnas+")VALUES("+valores+")";
+            string sql = "INSERT INTO "+tabla+" ("+columnas+")VALUES("+valores+")";
             cmd = new SqlCommand(sql, Conexion.conectar());
             int i = cmd.ExecuteNonQuery();
             Conexion.desconectar();
@@ -42,8 +61,9 @@ namespace SistemadeControlPoliciaco
                 return false;
             }
         }
-        public bool eliminar(string sql)
+        public bool eliminar(string tabla, string columna, string valor)
         {
+            string sql = "DELETE FROM " + tabla + " WHERE " + columna + " = " + valor;
             cmd = new SqlCommand(sql, Conexion.conectar());
             int i = cmd.ExecuteNonQuery();
             Conexion.desconectar();
@@ -56,8 +76,9 @@ namespace SistemadeControlPoliciaco
                 return false;
             }
         }
-        public bool modificar(string sql)
+        public bool modificar(string tabla, string columnaM, string valorM, string columna, string valor)
         {
+            string sql = "UPDATE " + tabla + " SET " + columnaM + " = " + valorM+" WHERE "+columna+" = "+valor;
             cmd = new SqlCommand(sql, Conexion.conectar());
             int i = cmd.ExecuteNonQuery();
             Conexion.desconectar();
