34f4f6b2e9b1638c00adca46871d77349ccf4d4d
Luis Alberto Ruiz <luisalbertoruiz.geek@gmail.com>
2014-09-20 10:29:48 -0500

    Grego clases Limpiar Vacio y Validar

---
+++ SistemadeControlPoliciaco/Clases/Limpiar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemadeControlPoliciaco
+{
+    public class Limpiar
+    {
+        public static void txb(Form frm)
+        {
+            foreach (Control ctrl in frm.Controls)
+            {
+                if (ctrl is TextBox)
+                {
+                    ctrl.Text = "";
+                }
+            }
+        }       
+    }
+}
---
+++ SistemadeControlPoliciaco/Clases/Vacio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemadeControlPoliciaco
+{
+    public class Vacio
+    {
+        public static int bo;
+        public static bool txb(Form frm)
+        {
+
+            foreach (Control ctrl in frm.Controls)
+            {
+                if (ctrl is TextBox && ctrl.Text == String.Empty)
+                {
+                    bo = 0;
+                    break;
+                }
+                else
+                {
+                    bo = 1;
+                }
+            }
+            if (bo > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
