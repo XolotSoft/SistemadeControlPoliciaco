c5a37918b36de1881a76963aa8885010bfa630d2
vicogh <bludemonjr@yahoo.com.mx>
2014-09-21 16:16:42 -0500

    Agregue formularios de registro contacto foto huella

---
+++ SistemadeControlPoliciaco/Foto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemadeControlPoliciaco
+{
+    public partial class Foto : Form
+    {
+        public Foto()
+        {
+            InitializeComponent();
+        }
+    }
+}
